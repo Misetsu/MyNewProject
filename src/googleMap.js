a7beec2c276f1a0d095359674088ffaa4ac3d354
HandaRyo <kd1335461@st.kobedenshi.ac.jp>
2024-06-14 15:03:53 +0900

    現在地表示（未完成）2024/06/14

--- src/googleMap.js
+++ src/googleMap.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, StyleSheet } from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import Geolocation, {GeolocationResponse} from 'react-native-geolocation-service';
 
-export default class Map extends React.Component {
-    render() {
-        return (
-            <View style={{ height: "100%", backgroundColor: "#DDDDDD", flexDirection: "column"}}>
-                <MapView style={{...StyleSheet.absoluteFillObject}}
-                provider="google"
-                ref={(ref) => {this.mapRef = ref}}
-                initialRegion={{
-                    latitude: 34.694,
-                    longitude: 135.193,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}>
-                    {/* マップ上ピン打ち latitude, longitudeに座標*/}
-                    <Marker
-                        coordinate={{latitude: 34.694, longitude: 135.193}}
-                        title="this is a marker"
-                        description="this is a marker example"
-                    />
-                    
-                </MapView>
-            </View>
-        )
-    }
+const coords = () => {
+    const [location, setLocation] = useState<GeolocationResponse['coords']>({
+        latitude: 0,
+        longitude: 0
+    });
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+        location => setLocation(location.coords),
+        err => alert(err.message),
+        {enableHighAccuracy: true, timeout: 10000},
+        );
+    }, []);
+
+    return location;
 }
+
+export default class Map extends React.Component{
+    render() {
+    
+    return (
+        <View style={{ height: "100%", backgroundColor: "#DDDDDD", flexDirection: "column"}}>
+            {coords && (
+            <MapView style={{...StyleSheet.absoluteFillObject}}
+            provider="google"
+            ref={(ref) => {this.mapRef = ref}}
+            initialRegion={{
+                latitude: 34.694,
+                longitude: 135.193,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }}>
+                {/* マップ上ピン打ち latitude, longitudeに座標*/}
+                <Marker
+                    coordinate={{latitude: 34.694, longitude: 135.193}}
+                    title="this is a marker"
+                    description="this is a marker example"
+                />
+            </MapView>
+            )}
+        </View>
+    )
+    }
+};
 
 
---
+++ src/location.js
@@ -0,0 +1,20 @@
+import Geolocation, {GeolocationResponse} from 'react-native-geolocation-service';
+
+export const CurrentPosition = () => {
+  const [location, setLocation] = useState<GeolocationResponse['coords']>({
+    latitude: 0,
+    longitude: 0
+  });
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      location => setLocation(location.coords),
+      err => alert(err.message),
+      {enableHighAccuracy: true, timeout: 10000},
+    );
+  }, []);
+
+  return (
+    location
+  )
+}
