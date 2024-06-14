cba9b96cde28cde35350fd7fe5929981a5d696da
Misetsu <nakitamak@gmail.com>
2024-06-14 09:58:21 +0900

    import property

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import {Button, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
