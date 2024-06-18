1972de947c610f30d2558461a767236d7c10937a
Kenta <kd1326756@st.kobedenshi.ac.jp>
2024-06-18 13:27:08 +0900

    Android

--- App.jsx
+++ App.jsx
@@ -1,61 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import React, { Component } from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { AppRegistry, ScrollView, Text, StyleSheet} from 'react-native';
 
+styles=StyleSheet.create({
+    scroller: {
+        flex: 1,
+    }
+});
 
-export default class HelloWorldApp extends Component {  //コンポーネントは部品的な意味がある
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: "タンクトップマスター",
-      latitude: null,  // 追加: 緯度の状態を保持する
-      longitude: null, // 追加: 経度の状態を保持する
-      error: null      // 追加: エラーの状態を保持する
-    };
-  }
-  
-  
-
-  
-  render() {  //render()メソッドは、そのコンポーネントがどのように描画されるかを定義する。
-    return (
-      <View style={styles.container}>
-        <Text style={styles.textEn}>NEWS</Text>
-        <Text style={{ fontSize: 20 }}>{this.state.data}</Text>
-        <Text>お試し追加ですよ！！</Text>
-        <Button
-                title="現在地取得"
-                color="red"
-            />
-      </View>
+export default class AndroidFonts extends Component{
+  render (){
+    return(
+      <ScrollView style={styles.scroller}>
+        <Text style={{fontFamily: 'normal'}}>  normal </Text>
+        <Text style={{fontFamily: 'notoserif'}}>  notoserif </Text>
+        <Text style={{fontFamily: 'sans-serif'}}>  sans-serif </Text>
+        <Text style={{fontFamily: 'sans-serif-light'}}>  sans-serif-light </Text>
+        <Text style={{fontFamily: 'sans-serif-thin'}}>  sans-serif-thin </Text>
+        <Text style={{fontFamily: 'sans-serif-condensed'}}>  sans-serif-condensed </Text>
+        <Text style={{fontFamily: 'sans-serif-medium'}}>  sans-serif-medium </Text>
+        <Text style={{fontFamily: 'serif'}}>  serif </Text>
+        <Text style={{fontFamily: 'Roboto'}}>  Roboto </Text>
+        <Text style={{fontFamily: 'monospace'}}>  monospace </Text>        
+      </ScrollView>
     );
-    //<View>はReact Nativeでのレイアウトコンポーネント
-    //<View>はフレックスボックスのように機能し、justifyContentとalignItemsプロパティを使用して、子要素を配置する。
-    //<Text>はテキストを表示するためのコンポーネント
   }
-  
 }
 
-//Stylesheet
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textEn:{
-    fontSize: 30,
-    fontWeight:'300',
-    fontFamily: 'sans-serif-thin',
-  },
-  textJa:{
-
-  }
-
-});
+AppRegistry.registerComponent('AndroidFonts', () => AndroidFonts);
